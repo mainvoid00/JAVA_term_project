f55035fa70fb2d9256c228ab0bef877b67cd4540
mainvoid00 <133748782+mainvoid00@users.noreply.github.com>
2023-05-17 17:09:31 +0900

    Add files via upload

---
+++ swing/src/swing/MyActionListener.java
@@ -0,0 +1,9 @@
+package swing;
+import java.awt.event.ActionEvent;
+
+interface ActionListener {
+	public void actionPerformed(ActionEvent e)
+}
+public class MyActionListener {
+
+}
---
+++ swing/src/swing/MyFrame.java
@@ -0,0 +1,14 @@
+package swing;
+import java.awt.Image;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		super("타이틀 문자열 ");
+		setTitle("관리자 모드");
+		setSize(500,500);
+		setVisible(true);
+	
+	}
+}
